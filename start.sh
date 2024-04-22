ab8617fa4b1ef84aa396e38c804e4dce9f32dc0a
Awnrt <roman.levikoff@gmail.com>
2024-04-22 04:23:05 +0300

    uploaded sum scripts

--- start.sh
+++
@@ -1,7 +0,0 @@
-export XDG_RUNTIME_DIR=/tmp/xdg-runtime-$(id -u)
-mkdir -p $XDG_RUNTIME_DIR
-#export WLR_RENDERER_ALLOW_SOFTWARE=1
-#dwl -s 'dwlb -font "JetBrainsMonoNerdFontMono:size=12"'
-someblocks -p | dwlb -status-stdin all &
-dwl -s 'dwlb -font "JetBrainsMonoNerdFontMono:size=12"'
-
